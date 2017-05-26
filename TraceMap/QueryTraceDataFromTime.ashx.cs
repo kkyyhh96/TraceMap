768cea100eca7958a38753b4d8b3edf5b0d6dcc6
kyh <kkyyhh96@163.com>
2017-05-26 10:22:37 +0800

    5-26

--- TraceMap/QueryTraceDataFromTime.ashx.cs
+++ TraceMap/QueryTraceDataFromTime.ashx.cs
@@ -13,19 +13,17 @@ namespace TraceMap
     {
         override protected string QueryTrace(HttpContext context, string result)
         {
-            string start_time_Y = context.Request.QueryString["start_time_Y"];
-            string start_time_M = context.Request.QueryString["start_time_M"];
-            string start_time_D = context.Request.QueryString["start_time_D"];
-            string start_time_H = context.Request.QueryString["start_time_H"];
-            string start_time_m = context.Request.QueryString["start_time_m"];
-            string start_time = new TimeStamp(start_time_Y, start_time_M, start_time_D, start_time_H, start_time_m).GetTime;
+            string start_time_Y = context.Request.QueryString["sY"];
+            string start_time_M = context.Request.QueryString["sM"];
+            string start_time_D = context.Request.QueryString["sD"];
+            string start_time_H = context.Request.QueryString["sH"];
+            string start_time = new TimeStamp(start_time_Y, start_time_M, start_time_D, start_time_H).GetTime;
 
-            string end_time_Y = context.Request.QueryString["end_time_Y"];
-            string end_time_M = context.Request.QueryString["end_time_M"];
-            string end_time_D = context.Request.QueryString["end_time_D"];
-            string end_time_H = context.Request.QueryString["end_time_H"];
-            string end_time_m = context.Request.QueryString["end_time_m"];
-            string end_time = new TimeStamp(end_time_Y, end_time_M, end_time_D, end_time_H, end_time_m).GetTime;
+            string end_time_Y = context.Request.QueryString["eY"];
+            string end_time_M = context.Request.QueryString["eM"];
+            string end_time_D = context.Request.QueryString["eD"];
+            string end_time_H = context.Request.QueryString["eH"];
+            string end_time = new TimeStamp(end_time_Y, end_time_M, end_time_D, end_time_H).GetTime;
 
             string username = context.Request.QueryString["username"];
             int id = Convert.ToInt32(username);
@@ -36,7 +34,7 @@ namespace TraceMap
             connection.Open();
 
             //构建SQL语句进行查询
-            string sqlCommand = String.Format("SELECT lat,lon FROM trace WHERE person = {0} AND t > {1} AND t < {2} ORDER BY t", id, start_time, end_time);
+            string sqlCommand = String.Format("SELECT lat,lon FROM trace WHERE person = {0} AND t > '{1}' AND t < '{2}' ORDER BY t", id, start_time, end_time);
             NpgsqlCommand command = new NpgsqlCommand(sqlCommand, connection);
             NpgsqlDataReader reader = command.ExecuteReader();
 
@@ -54,20 +52,19 @@ namespace TraceMap
 
         public class TimeStamp
         {
-            int year, month, day, hour, minute;
-            public TimeStamp(string year, string month, string day, string hour, string minute)
+            int year, month, day, hour;
+            public TimeStamp(string year, string month, string day, string hour)
             {
                 this.year = Convert.ToInt32(year);
                 this.month = Convert.ToInt32(month);
                 this.day = Convert.ToInt32(day);
                 this.hour = Convert.ToInt32(hour);
-                this.minute = Convert.ToInt32(minute);
             }
             public string GetTime
             {
                 get
                 {
-                    return String.Format("{0}-{1}-{2} {3}:{4}", year, month, day, hour, minute);
+                    return String.Format("{0}-{1}-{2} {3}:00", year, month, day, hour);
                 }
             }
         }
